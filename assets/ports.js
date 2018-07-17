4292034cffcc0863ff8145655aadaca2373a544a
Evan <github@erosson.org>
2018-07-17 04:39:32 -0400

    add google analytics

--- assets/ports.js
+++ assets/ports.js
@@ -1,5 +1,10 @@
-console.log(window.helpfulAdventurer)
+// google analytics
+window.dataLayer = window.dataLayer || []
+function gtag(){dataLayer.push(arguments)}
+gtag('js', new Date())
+gtag('config', 'UA-122483662-1')
 
+console.log(window.helpfulAdventurer)
 var app = Elm.Main.fullscreen({
   // TODO support list of characterdata
   characterData: window.helpfulAdventurer,
