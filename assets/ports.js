dd7269315144384e1708f2c45f739f56ebca49df
Evan Rosson <git@erosson.org>
2020-05-19 04:07:15 -0400

    code formatting

--- assets/ports.js
+++ assets/ports.js
@@ -8,14 +8,14 @@ gtag("config", "UA-122483662-1");
 
 // load everything before elm inits, to avoid the trouble of loading states/ports
 Promise.all([
-  fetch("./ch2data/chars/all.min.json").then(function(res) {
+  fetch("./ch2data/chars/all.min.json").then(function (res) {
     return res.json();
   }),
-  fetch("./CHANGELOG.md").then(function(res) {
+  fetch("./CHANGELOG.md").then(function (res) {
     return res.text();
-  })
+  }),
 ])
-  .then(function([game, changelog]) {
+  .then(function ([game, changelog]) {
     // console.log(chars)
     var app = Elm.Main.init({
       node: document.documentElement,
@@ -24,13 +24,13 @@ Promise.all([
         gameData: game,
         windowSize: {
           width: document.documentElement.clientWidth,
-          height: document.documentElement.clientHeight
-        }
-      }
+          height: document.documentElement.clientHeight,
+        },
+      },
     });
-    app.ports.saveFileSelected.subscribe(function(elemId) {
+    app.ports.saveFileSelected.subscribe(function (elemId) {
       var reader = new FileReader();
-      reader.onload = function() {
+      reader.onload = function () {
         try {
           var file = reader.result;
           var inflate = new Zlib.RawInflate(file);
@@ -44,13 +44,13 @@ Promise.all([
             build: Object.keys(json.nodesPurchased),
             equippedEtherealItems: json.equippedEtherealItems,
             etherealItemInventory: json.etherealItemInventory,
-            etherealItemStorage: json.etherealItemStorage
+            etherealItemStorage: json.etherealItemStorage,
           });
         } catch (error) {
           console.error("Error while reading savefile", error);
           app.ports.saveFileContentRead.send({
             status: "error",
-            error: error.message
+            error: error.message,
           });
         }
       };
