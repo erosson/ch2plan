a322ceafacd2433bb02ddd4fc2ee5717b323a50f
Evan Rosson <git@erosson.org>
2020-07-22 16:05:57 -0400

    remove parcel website

--- www/src/index.js
+++ www/src/index.js
@@ -1,13 +1,9 @@
 import "./main.css";
 import { Zlib } from "zlibjs/bin/rawinflate.min.js";
 import AMF from "amf-js";
-import { Elm } from "./Main.elm";
+import { Elm } from "../src/Main.elm";
 import gameData from "../../assets/public/ch2data/chars/all.min.json";
-// I want to import raw text. fs.readFileSync() runs at build time,
-// which is confusing/surprising, but works:
-// https://github.com/parcel-bundler/parcel/issues/970
-import fs from "fs";
-const changelog = fs.readFileSync(__dirname + "/../../CHANGELOG.md").toString();
+import changelog from "raw-loader!../../CHANGELOG.md";
 
 // google analytics
 window.dataLayer = window.dataLayer || [];
--- www2/src/Ports.elm
+++
@@ -1,9 +0,0 @@
-port module Ports exposing (saveFileContentRead, saveFileSelected)
-
-import Json.Decode as D
-
-
-port saveFileSelected : String -> Cmd msg
-
-
-port saveFileContentRead : (D.Value -> msg) -> Sub msg
